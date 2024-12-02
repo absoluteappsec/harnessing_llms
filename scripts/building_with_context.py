e3784c43437891c96b20aa8a1b0322fdfe6cd9c8
Ken Johnson <cktricky@gmail.com>
2024-12-01 20:58:15 -0500

    remove unnecessary import

--- scripts/building_with_context.py
+++ scripts/building_with_context.py
@@ -2,7 +2,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableMap
 
 # Load Env Variables
 from dotenv import load_dotenv
